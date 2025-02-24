5a6fa9a8a67c6d9e9b88bda753e3995b019c8ae6
L1n <89698082+l1n-zh@users.noreply.github.com>
2025-02-24 23:55:58 +0800

    formatting

--- src/components/clock_triple/RoleSelect.vue
+++ src/components/clock_triple/RoleSelect.vue
@@ -1,9 +1,13 @@
 <template>
     <div class="h-full flex p-10">
         <div class="m-auto">
-            <button class="border-1 text-5xl rounded-xl p-6 px-15 font-light">成為螢幕</button>
-            <div class=" h-10"></div>
-            <button class="border-1 text-5xl rounded-xl p-6 px-15 font-light">成為按鈕</button>
+            <button class="border-1 text-5xl rounded-xl p-6 px-15 font-light">
+                成為螢幕
+            </button>
+            <div class="h-10"></div>
+            <button class="border-1 text-5xl rounded-xl p-6 px-15 font-light">
+                成為按鈕
+            </button>
         </div>
     </div>
 </template>
--- src/components/clock_triple/TimeSetting.vue
+++ src/components/clock_triple/TimeSetting.vue
@@ -1,31 +1,40 @@
 <template>
     <div class="flex gap-10">
-        <span @click="initial_time_select = true" class=" select-none">
+        <span @click="initial_time_select = true" class="select-none">
             <text class="block text-2xl text-gray-500">每方分鐘數</text>
             <span class="border-b-1 w-min mr-0.5">
                 <text class="text-6xl">{{ time_setting.initial_time }}</text>
             </span>
             <text class="text-2xl font-light">分鐘</text>
         </span>
-        <TimeSelect :options="[0, 3, 5, 10, 15, 20, 30, 60]" unit="分鐘" v-if="1" v-model:result="time_setting.initial_time"
-            v-model:enable="initial_time_select">
+        <TimeSelect
+            :options="[0, 3, 5, 10, 15, 20, 30, 60]"
+            unit="分鐘"
+            v-if="1"
+            v-model:result="time_setting.initial_time"
+            v-model:enable="initial_time_select"
+        >
         </TimeSelect>
-        <span @click="() => byoyomi_select = true" class="select-none">
+        <span @click="() => (byoyomi_select = true)" class="select-none">
             <text class="block text-2xl text-gray-500">讀秒</text>
             <span class="border-b-1 w-min mr-0.5">
                 <text class="text-6xl">{{ time_setting.byoyomi }}</text>
             </span>
             <text class="text-2xl font-light">秒</text>
         </span>
-        <TimeSelect :options="[0, 10, 15, 20, 30, 40, 50, 60]" unit="秒" v-if="1" v-model:result="time_setting.byoyomi"
-            v-model:enable="byoyomi_select"></TimeSelect>
+        <TimeSelect
+            :options="[0, 10, 15, 20, 30, 40, 50, 60]"
+            unit="秒"
+            v-if="1"
+            v-model:result="time_setting.byoyomi"
+            v-model:enable="byoyomi_select"
+        ></TimeSelect>
     </div>
-
 </template>
 
 <script setup>
-import { ref } from 'vue';
-import TimeSelect from './TimeSelect.vue';
+import { ref } from "vue";
+import TimeSelect from "./TimeSelect.vue";
 
 const initial_time_select = ref(false);
 const byoyomi_select = ref(false);
@@ -36,6 +45,6 @@ const time_setting = defineModel({
     },
     byoyomi: {
         type: Number,
-    }
-})
+    },
+});
 </script>
