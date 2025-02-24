7067e26fefef478bcbd75f0e85c424803784d112
L1n <89698082+l1n-zh@users.noreply.github.com>
2025-02-24 16:44:02 +0800

    add ClockSelect

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,11 @@
 <template>
+  <div class="h-[100vh]">
+    <ClockSelect></ClockSelect>
+  </div>
 </template>
 
 <script setup>
+  import ClockSelect from "./components/ClockSelect.vue";
 </script>
 
 <style scoped>
