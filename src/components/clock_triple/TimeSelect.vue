967ab7c4db92bf0c81456076877621f523733e90
L1n <89698082+l1n-zh@users.noreply.github.com>
2025-02-24 22:59:43 +0800

    finish UI/UX

--- src/components/clock_triple/TimeSelect.vue
+++ src/components/clock_triple/TimeSelect.vue
@@ -1,8 +1,12 @@
 <template>
-    <div class="flex flex-col">
+    <div class="absolute z-1 w-full h-full backdrop-blur-xs" @click="enable = false; result = selected_value"
+        v-show="enable">
+    </div>
+    <div class="flex flex-col absolute backdrop-blur-xs border-1 border-gray-100 rounded-md top-1/2 left-1/2 -translate-1/2 p-10 z-2 w-max max-w-10/12 backdrop-brightness-125 select-none"
+        v-show="enable">
         <div class="flex m-auto flex-wrap">
-            <div class="w-min px-4 relative text-5xl font-thin select-none" v-for="option in options"
-                @click="result = option">
+            <div class="w-min px-4 relative text-5xl font-thin" v-for="option in options"
+                @click="selected_value = option">
                 {{ option }}
                 <div
                     :class="[option != options.at(-1) ? 'border-r-1' : '', 'h-6 w-full absolute top-1/2 left-1/2 -translate-1/2']">
@@ -11,8 +15,8 @@
         </div>
         <div class="m-auto">
             <span class="mt-3 border-b-1">
-                <input class="text-center text-5xl font-thin w-[2em]" inputmode="numeric" v-model="result"
-                    @input="event => { result = verify_input(event.target.value) }">
+                <input class="text-center text-5xl font-thin w-[2em]" inputmode="numeric" v-model="selected_value"
+                    @input="event => { selected_value = verify_input(event.target.value) }">
             </span>
             <text class="font-light text-2xl">{{ unit }}</text>
         </div>
@@ -21,14 +25,22 @@
 
 <script setup>
 import { ref } from "vue";
+
 const options = [0, 3, 5, 10, 15, 20, 30, 60];
 
-defineProps({
+const props = defineProps({
     unit: String,
-    options: Array
+    options: Array,
+    enable: Boolean
 })
 
-const result = defineModel()
+const result = defineModel('result', {
+    type: Number
+})
+const selected_value = ref(result.value);
+const enable = defineModel('enable', {
+    type: Boolean
+})
 
 const verify_input = value => {
     if (isNaN(value)) return "";
--- src/components/clock_triple/TimeSetting.vue
+++ src/components/clock_triple/TimeSetting.vue
@@ -1,36 +1,41 @@
 <template>
     <div class="flex gap-10">
-        <span>
+        <span @click="initial_time_select = true" class=" select-none">
             <text class="block text-2xl text-gray-500">每方分鐘數</text>
             <span class="border-b-1 w-min mr-0.5">
                 <text class="text-6xl">{{ time_setting.initial_time }}</text>
             </span>
             <text class="text-2xl font-light">分鐘</text>
         </span>
-        <TimeSelect :options="[0, 3, 5, 10, 15, 20, 30, 60]" unit="分鐘" v-if="1" v-model="time_setting.initial_time"></TimeSelect>
-        <span>
+        <TimeSelect :options="[0, 3, 5, 10, 15, 20, 30, 60]" unit="分鐘" v-if="1" v-model:result="time_setting.initial_time"
+            v-model:enable="initial_time_select">
+        </TimeSelect>
+        <span @click="() => byoyomi_select = true" class="select-none">
             <text class="block text-2xl text-gray-500">讀秒</text>
             <span class="border-b-1 w-min mr-0.5">
                 <text class="text-6xl">{{ time_setting.byoyomi }}</text>
             </span>
             <text class="text-2xl font-light">秒</text>
         </span>
-        <TimeSelect :options="[0, 10, 15, 20, 30, 40, 50, 60]" unit="秒" v-if="1" v-model="time_setting.byoyomi"></TimeSelect>
+        <TimeSelect :options="[0, 10, 15, 20, 30, 40, 50, 60]" unit="秒" v-if="1" v-model:result="time_setting.byoyomi"
+            v-model:enable="byoyomi_select"></TimeSelect>
     </div>
 
 </template>
 
 <script setup>
+import { ref } from 'vue';
 import TimeSelect from './TimeSelect.vue';
+
+const initial_time_select = ref(false);
+const byoyomi_select = ref(false);
 
 const time_setting = defineModel({
     initial_time: {
         type: Number,
-        default: 10
     },
     byoyomi: {
         type: Number,
-        default: 30
     }
 })
 </script>
