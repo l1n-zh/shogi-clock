ebf9d49efd26283302ee21810700a184867e85a5
L1n <89698082+l1n-zh@users.noreply.github.com>
2025-02-24 16:45:54 +0800

    Add ClockSelect component with options for single, triple, and multi

--- src/components/ClockSelect.vue
+++ src/components/ClockSelect.vue
@@ -1,4 +1,19 @@
-<template></template>
+<template>
+    <div class="w-full h-full">
+        <text class=" block m-[3vh] font-extrabold text-gray-400 text-3xl">科摩多龍棋鐘</text>
+        <div class="grid grid-cols-3 gap-[3vh] mx-[3vh]">
+            <div class=" border-1">
+                <div class="aspect-square bg-gray-200"></div> <text class="block w-full text-center text-2xl">單機</text>
+            </div>
+            <div class="border-1">
+                <div class="aspect-square bg-gray-200"></div> <text class="block w-full text-center text-2xl">三機</text>
+            </div>
+            <div class="border-1">
+                <div class="aspect-square bg-gray-200"></div> <text class="block w-full text-center text-2xl">聯機</text>
+            </div>
+        </div>
+    </div>
+</template>
 
 <style scoped>
 </style>
