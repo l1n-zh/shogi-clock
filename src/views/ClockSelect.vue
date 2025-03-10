4e8772ee881fd3807e4e846fa9adbde31de8aa78
L1n <89698082+l1n-zh@users.noreply.github.com>
2025-03-10 19:59:27 +0800

    feat: router link

--- src/views/ClockSelect.vue
+++ src/views/ClockSelect.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="w-full h-full">
-        <text class="block m-[3vh] font-extrabold text-gray-400 text-3xl"
+        <text class="block m-[3vh] font-bold text-gray-400 text-3xl"
             >科摩多龍棋鐘</text
         >
         <div class="grid grid-cols-3 gap-[3vh] mx-[3vh]">
@@ -8,10 +8,12 @@
                 <div class="aspect-square bg-gray-200"></div>
                 <text class="block w-full text-center text-2xl">單機</text>
             </div>
-            <div class="border-1">
-                <div class="aspect-square bg-gray-200"></div>
-                <text class="block w-full text-center text-2xl">三機</text>
-            </div>
+            <RouterLink to="/triple">
+                <div class="border-1">
+                    <div class="aspect-square bg-gray-200"></div>
+                    <text class="block w-full text-center text-2xl">三機</text>
+                </div>
+            </RouterLink>
             <div class="border-1">
                 <div class="aspect-square bg-gray-200"></div>
                 <text class="block w-full text-center text-2xl">聯機</text>
--- src/views/clock_triple/RoleSelect.vue
+++ src/views/clock_triple/RoleSelect.vue
@@ -1,13 +1,12 @@
 <template>
     <div class="h-full flex sm:p-10 md:text-5xl text-4xl p-5 font-light">
         <div class="m-auto">
-            <button class="border-1 rounded-xl p-6 px-15 ">
-                成為螢幕
-            </button>
-            <div class="h-10"></div>
-            <button class="border-1 rounded-xl p-6 px-15 ">
-                成為按鈕
-            </button>
+            <RouterLink to="/triple/display">
+                <button class="border-1 rounded-xl p-6 px-15 mb-10">成為螢幕</button>
+            </RouterLink>
+            <RouterLink to="/triple/pad">
+                <button class="border-1 rounded-xl p-6 px-15">成為按鈕</button>
+            </RouterLink>
         </div>
     </div>
 </template>
