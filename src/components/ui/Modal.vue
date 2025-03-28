f722fd13ea33a88e3cf3bf57304b78cacc6d1661
L1n <89698082+l1n-zh@users.noreply.github.com>
2025-03-28 21:40:23 +0800

    bug fix

--- src/components/ui/Modal.vue
+++ src/components/ui/Modal.vue
@@ -17,44 +17,54 @@
 </template>
 
 <script setup>
-import { ref, onMounted, watch, onUnmounted } from 'vue';
+import { ref, onMounted, watch, onUnmounted } from "vue";
 
 const props = defineProps({
-  activator: Object,
+    activator: Object,
 });
 
 const visible = ref(false);
 let activator_el = null;
 
 const toggle_visibility = () => {
-  visible.value = !visible.value;
+    visible.value = !visible.value;
+};
+
+const get_activator_el = (activator) => {
+    if (activator.$el) {
+        return activator.$el;
+    }
+    if (activator instanceof HTMLElement) {
+        return activator;
+    }
+    return null;
+};
+
+const remove_listener = () => {
+    if (activator_el) {
+        activator_el.removeEventListener("click", toggle_visibility);
+    }
 };
 
 onMounted(() => {
-  watch(
-    () => props.activator,
-    (new_activator, old_activator) => {
-    console.log(new_activator, old_activator)
-      if (old_activator) {
-        old_activator.$el.removeEventListener('click', toggle_visibility);
-      }
-
-      if (new_activator) {
-        activator_el = new_activator.$el;
-        activator_el.addEventListener('click', toggle_visibility);
-      }
-    },
-    { immediate: true }
-  );
+    watch(
+        () => props.activator,
+        (new_activator, old_activator) => {
+            console.log(new_activator, old_activator);
+            remove_listener();
+            if (new_activator) {
+                activator_el = get_activator_el(new_activator);
+                activator_el.addEventListener("click", toggle_visibility);
+            }
+        },
+        { immediate: true }
+    );
 });
 
 onUnmounted(() => {
-  if (activator_el) {
-    activator_el.removeEventListener('click', toggle_visibility);
-  }
+    remove_listener();
 });
 </script>
-
 
 <style lang="postcss" scoped>
 .v-enter-active,
