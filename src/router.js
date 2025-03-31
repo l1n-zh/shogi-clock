cf25f91a2084b41f496a095493860cd512ab06c2
L1n <89698082+l1n-zh@users.noreply.github.com>
2025-03-31 17:11:24 +0800

    feat: Add route for single clock view

--- src/router.js
+++ src/router.js
@@ -5,12 +5,15 @@ import {
 } from "vue-router";
 
 import ClockSelect from "./views/ClockSelect.vue";
+import ClockSingleView from "./views/clock_single/ClockView.vue";
+
 import RoleSelect from "./views/clock_triple/RoleSelect.vue";
 import ButtonView from "./views/clock_triple/ButtonView.vue";
 import DisplayView from "./views/clock_triple/DisplayView.vue";
 
 const routes = [
     { path: "/", component: ClockSelect },
+    { path: "/single", component: ClockSingleView },
     { path: "/triple", component: RoleSelect },
     { path: "/triple/display", component: DisplayView },
     { path: "/triple/button", component: ButtonView },
